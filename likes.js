6e9c9cfea684864102b606017c9357d7b487dc6c
wanderlust101 <76796111+wanderlust101@users.noreply.github.com>
2021-08-25 15:46:26 -0400

    Add files via upload

---
+++ likes.js
@@ -0,0 +1,22 @@
+var likesone = document.querySelector(".likesone");
+var likestwo = document.querySelector(".likestwo");
+var likesthree = document.querySelector(".likesthree")
+
+function likeone() {
+
+    likesone.innerText ++
+}
+
+var likes = document.querySelector(".likestwo");
+
+function liketwo() {
+
+    likestwo.innerText ++
+}
+
+var likes = document.querySelector(".likestwo");
+
+function likethree() {
+
+    likesthree.innerText ++
+}
